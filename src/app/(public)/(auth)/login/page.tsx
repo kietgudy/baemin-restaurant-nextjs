4b32826d042a5e8d2c32f444b1e1b9c3deb4a61b
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-24 01:23:33 +0700

    design page login

---
+++ src/app/(public)/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/app/(public)/(auth)/login/login-form";
+
+export default function Login() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <LoginForm />
+    </div>
+  );
+}
