98fd2c2fd4269a9e03702c54e9f1c4589109db07
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-25 21:36:07 +0700

    middleware nextjs

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const privatePaths = ['/manage']
+const loggedInPaths = ['/login']
+ 
+export function middleware(request: NextRequest) {
+  const {pathname} = request.nextUrl
+  const isAuth = Boolean(request.cookies.get('accessToken')?.value)
+  // Chưa đăng nhập thì không cho vào privatePaths
+  if (privatePaths.some(path =>pathname.startsWith(path)) && !isAuth) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+  // Đã đăng nhập thì không cho vào login
+  if (loggedInPaths.some(path =>pathname.startsWith(path)) && isAuth) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+  return NextResponse.next()
+}
+ 
+export const config = {
+  matcher: ['/manage/:path*', '/login'],
+}
