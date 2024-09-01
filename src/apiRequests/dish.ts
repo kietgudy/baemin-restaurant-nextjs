3bebac18ab50305d4138311a45da2e68efff2c0a
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-09-01 23:30:32 +0700

    declare API dishes

---
+++ src/apiRequests/dish.ts
@@ -0,0 +1,20 @@
+import http from "@/lib/http";
+import {
+  CreateDishBodyType,
+  DishListResType,
+  DishResType,
+  UpdateDishBodyType,
+} from "@/schemaValidations/dish.schema";
+
+const dishApiRequest = {
+  // Next.js 15 mặc định fetch { cache: 'no-store' } (dynamic rendering page)
+  // Next.js 14 mặc định fetch { cache: 'force-cache' } nghĩa là cache (static rendering page)
+  list: () => http.get<DishListResType>("dishes"),
+  add: (body: CreateDishBodyType) => http.post<DishResType>("dishes", body),
+  getDish: (id: number) => http.get<DishResType>(`dishes/${id}`),
+  updateDish: (id: number, body: UpdateDishBodyType) =>
+    http.put<DishResType>(`dishes/${id}`, body),
+  deleteDish: (id: number) => http.delete<DishResType>(`dishes/${id}`),
+};
+
+export default dishApiRequest;
