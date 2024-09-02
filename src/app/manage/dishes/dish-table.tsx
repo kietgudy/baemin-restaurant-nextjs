b0233490aa54655a2be7e4241bdf53719545a6f4
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-09-02 17:44:22 +0700

    the function delete a dish

--- src/app/manage/dishes/dish-table.tsx
+++ src/app/manage/dishes/dish-table.tsx
@@ -44,14 +44,15 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
-import { formatCurrency, getVietnameseDishStatus } from "@/lib/utils";
+import { formatCurrency, getVietnameseDishStatus, handleErrorApi } from "@/lib/utils";
 import { useSearchParams } from "next/navigation";
 import AutoPagination from "@/components/auto-pagination";
 import { DishListResType } from "@/schemaValidations/dish.schema";
 import EditDish from "@/app/manage/dishes/edit-dish";
 import AddDish from "@/app/manage/dishes/add-dish";
-import { useDishListQuery } from "@/queries/useDish";
+import { useDeleteDishMutation, useDishListQuery } from "@/queries/useDish";
 import DOMPurify from "dompurify";
+import { toast } from "@/components/ui/use-toast";
 
 type DishItem = DishListResType["data"][0];
 
@@ -156,6 +157,22 @@ function AlertDialogDeleteDish({
   dishDelete: DishItem | null;
   setDishDelete: (value: DishItem | null) => void;
 }) {
+  const {mutateAsync} = useDeleteDishMutation()
+  const deleteDish = async () => {
+    if(dishDelete) {
+      try {
+        const result = await mutateAsync(dishDelete.id)
+        setDishDelete(null)
+        toast({
+          title: `${result.payload.message} ${dishDelete.name}`,
+        })
+      } catch (error) {
+        handleErrorApi({
+          error,
+        })
+      }
+    }
+  }
   return (
     <AlertDialog
       open={Boolean(dishDelete)}
@@ -178,7 +195,7 @@ function AlertDialogDeleteDish({
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction onClick={() => deleteDish()}>Continue</AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
