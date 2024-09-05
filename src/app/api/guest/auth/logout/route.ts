79d952533e990a7ca7bca39716ce6eb38ca42dde
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-09-05 19:57:23 +0700

    declare route handler for auth guest

---
+++ src/app/api/guest/auth/logout/route.ts
@@ -0,0 +1,30 @@
+
+import guestApiRequest from "@/apiRequests/guest";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request) {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
+  cookieStore.delete("accessToken")
+  cookieStore.delete("refreshToken")
+  if(!accessToken || !refreshToken) {
+    return Response.json({
+      message: 'Token không hợp lệ'
+    }, {
+      status: 200
+    })
+  }
+  try {
+    const result = await guestApiRequest.serverLogout({
+        accessToken, refreshToken
+    })
+    return Response.json(result.payload)
+  } catch (error) {
+   return Response.json({
+        message: 'Đã có lỗi xảy ra'
+        }, {
+        status: 200
+   })
+  }
+}
