f2790542ef9611fa2835c0ccb0294c7ee8b6625e
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-09-09 00:40:43 +0700

    integrate socket.io to get realtime order status

--- src/app/guest/orders/orders-cart.tsx
+++ src/app/guest/orders/orders-cart.tsx
@@ -1,19 +1,44 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
+import socket from "@/lib/socket";
 import { formatCurrency, getVietnameseOrderStatus } from "@/lib/utils";
 import { useGuestOrderListQuery } from "@/queries/useGuest";
+import { UpdateOrderResType } from "@/schemaValidations/order.schema";
 import Image from "next/image";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 
 const OrdersCart = () => {
-  const { data } = useGuestOrderListQuery();
-  const orders = data?.payload.data ?? [];
+  const { data, refetch } = useGuestOrderListQuery();
+  const orders = useMemo(() => data?.payload.data ?? [], [data]);
   const totalPrice = useMemo(() => {
     return orders.reduce((total, order) => {
       return total + order.dishSnapshot.price * order.quantity;
     }, 0);
   }, [orders]);
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {}
+
+    function onDisconnect() {}
+
+    function onUpdateOrder(data: UpdateOrderResType['data']) {
+      refetch();
+    }
+    
+    socket.on("update-order", onUpdateOrder);
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
   return (
     <>
       {orders.map((order, index) => (
@@ -40,7 +65,9 @@ const OrdersCart = () => {
             </p>
           </div>
           <div className="flex-shrink-0 ml-auto flex justify-center items-center">
-            <Badge variant={"secondary"}>{getVietnameseOrderStatus(order.status)}</Badge>
+            <Badge variant={"secondary"}>
+              {getVietnameseOrderStatus(order.status)}
+            </Badge>
           </div>
         </div>
       ))}
