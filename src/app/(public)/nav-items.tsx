5f7fdb6a12a3263e1e40bbf4d923611bae01e623
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-09-09 01:01:36 +0700

    toast when user logout

--- src/app/(public)/nav-items.tsx
+++ src/app/(public)/nav-items.tsx
@@ -7,6 +7,17 @@ import { useLogoutMutation } from "@/queries/useAuth";
 import { RoleType } from "@/types/jwt.types";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const menuItems: {
   title: string;
@@ -74,12 +85,29 @@ export default function NavItems({ className }: { className?: string }) {
         return null;
       })}
       {role && (
-        <div
-          className={cn(className, "cursor-pointer")}
-          onClick={() => handleLogout()}
-        >
-          Đăng xuất
-        </div>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <div
+              className={cn(className, "cursor-pointer")}
+            >
+              Đăng xuất
+            </div>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Bạn có muốn đăng xuất không?</AlertDialogTitle>
+              <AlertDialogDescription>
+                Việc đăng xuất sẽ không lưu hóa đơn của bạn
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Hủy</AlertDialogCancel>
+              <AlertDialogAction onClick={() => handleLogout()}>
+                OK
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       )}
     </>
   );
