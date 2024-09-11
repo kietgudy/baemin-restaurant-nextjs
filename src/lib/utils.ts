2511c5dc68a4cdf1e87fef253fc86e1817ce3af4
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-09-12 02:01:44 +0700

    create UI manager order

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,9 +4,11 @@ import { twMerge } from "tailwind-merge";
 import { EntityError } from "./http";
 import { toast } from "@/components/ui/use-toast";
 import { DishStatus, OrderStatus, TableStatus } from "@/constants/type";
+import { BookX, CookingPot, HandCoins, Loader, Truck } from 'lucide-react'
 import jwt from "jsonwebtoken";
 import envConfig from "@/config";
 import { TokenPayload } from "@/types/jwt.types";
+import { format } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -117,3 +119,31 @@ export const getTableLink = ({
 export const decodeToken = (token: string) => {
   return jwt.decode(token) as TokenPayload;
 };
+
+export function removeAccents(str: string) {
+  return str
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/đ/g, 'd')
+    .replace(/Đ/g, 'D')
+}
+
+export const simpleMatchText = (fullText: string, matchText: string) => {
+  return removeAccents(fullText.toLowerCase()).includes(removeAccents(matchText.trim().toLowerCase()))
+}
+
+export const formatDateTimeToLocaleString = (date: string | Date) => {
+  return format(date instanceof Date ? date : new Date(date), 'HH:mm:ss dd/MM/yyyy')
+}
+
+export const formatDateTimeToTimeString = (date: string | Date) => {
+  return format(date instanceof Date ? date : new Date(date), 'HH:mm:ss')
+}
+
+export const OrderStatusIcon = {
+  [OrderStatus.Pending]: Loader,
+  [OrderStatus.Processing]: CookingPot,
+  [OrderStatus.Rejected]: BookX,
+  [OrderStatus.Delivered]: Truck,
+  [OrderStatus.Paid]: HandCoins
+}
